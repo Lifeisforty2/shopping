0a3e47c881ff0e7cde9d7c9d4e38b397d24d12ba
LifeisFortyTwo <75262374+Lifeisforty2@users.noreply.github.com>
2023-11-09 15:30:28 -0600

    started a logout feature

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 
-'''
-# Create your views here.
-def login_view(request):
-    return render(request, 'shop/login.html')
-'''
+def logout_view(request):
+    logout(request)
+    return redirect("index")
--- shopping_project/urls.py
+++ shopping_project/urls.py
@@ -17,12 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth  import views as auth_views
-
-
+from shop.views import logout_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path('logout/', logout_view, name='logout'),
     #...
 
 ]
