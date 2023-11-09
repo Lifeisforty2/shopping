eb13f46965929e11fb29c058e56f741205eeac5f
LifeisFortyTwo <75262374+Lifeisforty2@users.noreply.github.com>
2023-11-09 11:46:44 -0600

    Initial commit

---
+++ shop/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+
+'''
+# Create your views here.
+def login_view(request):
+    return render(request, 'shop/login.html')
+'''
