4ea4bdc28df8032b64bf1b7064066d7d83967fdf
sarahhalve <sarah.halverson@hotmail.com>
2023-11-10 13:32:34 -0600

    initial commit on fork

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
-<<<<<<< HEAD
-
-def logout_view(request):
-    logout(request)
-    return redirect("index")
-=======
 def home_view(request):
     return render(request, "home.html")
->>>>>>> 796bd902aeab841c3926976a4886b744bd086395
